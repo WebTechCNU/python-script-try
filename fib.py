b1c1e632e1cdc37dfa09bba614dee875a82cb251
maxvonlancaster <windchange7@gmail.com>
2024-04-19 00:42:58 +0300

    init

---
+++ fib.py
@@ -0,0 +1,23 @@
+# import sys
+
+# def fibonacci(n):
+#     if n <= 0:
+#         return "Invalid input. n must be a positive integer."
+
+#     fib_sequence = [0, 1]
+#     while len(fib_sequence) <= n:
+#         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+#     return fib_sequence[n]
+
+# if __name__ == "__main__":
+#     if len(sys.argv) != 2:
+#         print("Usage: python fibonacci.py <n>")
+#         sys.exit(1)
+
+#     try:
+#         n = int(sys.argv[1])
+#         result = fibonacci(n)
+#         print(result)
+#     except ValueError:
+#         print("Invalid input. Please provide a positive integer value for n.")
